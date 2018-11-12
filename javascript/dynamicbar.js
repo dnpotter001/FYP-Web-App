bcd6c930f6335afd4532c586a06df0b80f3ef736
David Potter <dnpotter001@gmail.com>
2018-11-12 12:32:49 +0000

    added internal padding and labels to the canvas element

--- javascript/dynamicbar.js
+++ javascript/dynamicbar.js
@@ -1,17 +1,25 @@
 //gets the canvas from html file
 const chartArea = document.getElementById("chart");
-//gets the device pixel ratio and size of canvas
-const dpr = window.devicePixelRatio || 1;
-const rect = chartArea.getBoundingClientRect();
 
-console.log(dpr);
 
-let value = [12,3,4,5,9];
+function setupCanvas(canvas) {
+  var dpr = window.devicePixelRatio || 1;
+  var rect = canvas.getBoundingClientRect();
+  canvas.width = rect.width * dpr;
+  canvas.height = rect.height * dpr;
+  var ctx = canvas.getContext('2d');
+  //ctx.scale(dpr, dpr);
+  //ctx.translate(0.25, 0.25);
+
+  return ctx;
+}
+
+let value = [12,3,4,5,9,13];
 
 function drawLine(ctx, startX, startY, endX, endY, colour){
   ctx.save();
   ctx.strokeStyle = colour;
-  ctx.lineWidth = 2 * dpr;
+  ctx.lineWidth = 2;
   ctx.beginPath();
   ctx.moveTo(startX,startY);
   ctx.lineTo(endX, endY);
@@ -22,17 +30,19 @@ function drawLine(ctx, startX, startY, endX, endY, colour){
 function drawBar(ctx, upperLX, upperLY, width, height, colour, label){
   ctx.save();
   ctx.fillStyle = colour;
+  ctx.font = "bold 20px Arial";
+  ctx.textAlign ="center";
+
   ctx.fillRect(upperLX, upperLY, width, height);
-  ctx.restore;
+  ctx.fillText(label, upperLX+(width/2), upperLY-5);
+  ctx.restore();
 }
 
-let BarChart = function(canvas, data, gridScale, seriesName, padding){
-  
+let BarChart = function(canvas, data, gridScale, seriesName, padding, barGap){
  
   this.canvas = canvas;
-  this.ctx = canvas.getContext("2d");
-  this.canvas.width = rect.width * dpr;
-  this.canvas.height = rect.height * dpr;
+  this.ctx = setupCanvas(this.canvas);
+  
   
   //sets the max value
   this.maxValue = 0;
@@ -42,33 +52,29 @@ let BarChart = function(canvas, data, gridScale, seriesName, padding){
     }
   }
 
-  let canvasW = this.canvas.width - padding;
+  let canvasW = this.canvas.width- padding;
   let canvasH = this.canvas.height - padding;
 
   //function for drawing grid lines
   this.DrawGridLine = function(){
     let gridValue = 0; 
-    let lineNo = 0;
-    let numberOfLines = this.maxValue / gridScale;
-    let lineGap = (canvasH / numberOfLines)-10;
     while(gridValue <= this.maxValue){
-      let yHeight = (canvasH) * (1 - gridValue/this.maxValue) +  padding;
+      let yHeight = (canvasH) * (1 - gridValue/this.maxValue) + padding;
       drawLine(
         this.ctx,
-        0,
-        yHeight,
-        canvasW,
-        yHeight,
+        0 + padding,
+        yHeight, //-padding,
+        canvasW ,
+        yHeight,// -padding,
         "#000000"
       );
 
       //adds labels to lines 
       this.ctx.save();
-      this.ctx.font = "bold " + 10 * dpr + "px Arial";
+      this.ctx.font = "bold 15px Arial";
       console.log(gridValue);
       this.ctx.fillText(gridValue, 0, yHeight);
       this.ctx.restore();
-      lineNo++
 
       gridValue = gridValue + gridScale;
     }
@@ -77,31 +83,42 @@ let BarChart = function(canvas, data, gridScale, seriesName, padding){
   this.drawBars = function(){
     let barNo = 0;
     let barWidth = canvasW / data.length;
-    let barIncrement = canvasH / this.maxValue;
+    let barIncrement = (canvasH / this.maxValue) ;
 
     for(x in data){
       drawBar(
         this.ctx,
-        barNo * barWidth, 
-        (this.maxValue - data[x])*barIncrement, 
-        barWidth, 
+        barNo * barWidth + padding, 
+        (this.maxValue - data[x])*barIncrement +padding , 
+        barWidth - (barGap *2), 
         data[x] *barIncrement, 
         getRandomColor(),
-        x
+        data[x],
       );
       barNo++
     }
   }
+
+  this.drawLabel = function(){
+    this.ctx.save();
+    this.ctx.textBaseLine = "bottom";
+    this.ctx.textAlign = "center";
+    this.fillStyle = "#000000";
+    this.ctx.font= "bold 20px Arial";
+    this.ctx.fillText("Super chart", canvasW / 2, canvasH);
+  }
+
 
 
 
   
 }
 
-let chart = new BarChart(chartArea, value, 2, "chart", 10);
+let chart = new BarChart(chartArea, value, 2, "chart", 25, 10);
 console.log(chart.maxValue);
 chart.DrawGridLine();
 chart.drawBars();
+chart.drawLabel();
 
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
