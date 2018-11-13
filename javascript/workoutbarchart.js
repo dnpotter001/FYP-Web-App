c157818f514307ec7d39828e658d4f63fc85fc6e
David Potter <dnpotter001@gmail.com>
2018-11-13 11:20:16 +0000

    graph takes array from a workout and generate graph

--- javascript/workoutbarchart.js
+++ javascript/workoutbarchart.js
@@ -1,5 +1,26 @@
 //gets the canvas from html file
 const chartArea = document.getElementById("chart");
+const chartButton = document.getElementById("generateChart"); 
+
+chartButton.addEventListener("click", () => {
+  if(fileInput.value) {
+    let chart = new BarChart(
+      chartArea,
+      workout,
+      25, // grid scale 
+      25, //padding 
+      10, //bar gap
+    );
+
+    console.log(chart.maxValue);
+    chart.DrawGridLine();
+    chart.drawBars();
+
+  } else {
+    fileNameLabel.innerHTML = "Upload a CSV first";
+
+  }
+})
 
 
 function setupCanvas(canvas) {
@@ -10,14 +31,25 @@ function setupCanvas(canvas) {
   canvas.width = rect.width * (2);
   canvas.height = rect.height * (2);
   ctx.scale(1, 1);
-  //old scaling
-  // canvas.width = rect.width * dpr;
-  // canvas.height = rect.height * dpr;
-  // ctx.scale(dpr, dpr);
   return ctx;
 }
 
-let value = [12,3,4,5,9,13];
+
+function getIntervalData(workout){
+  let graphData = [];
+  for(x in workout.intervals){
+    let intervals = workout.intervals[x]
+    console.log(workout.intervals[x]);
+    let timeArray = intervals[1].split(':');
+    console.log(timeArray);
+    let totalSecond = (timeArray[0] * 60) + ((timeArray[1]/100) * 60);
+    console.log(totalSecond);
+    graphData[x] = totalSecond;
+  }
+
+  console.log(graphData);
+  return graphData;
+}
 
 function drawLine(ctx, startX, startY, endX, endY, colour){
   ctx.save();
@@ -41,17 +73,17 @@ function drawBar(ctx, upperLX, upperLY, width, height, colour, label){
   ctx.restore();
 }
 
-let BarChart = function(canvas, data, gridScale, seriesName, padding, barGap){
-
+let BarChart = function(canvas, workout, gridScale, padding, barGap){
+  //take parameters and sets up canvas and data
   this.canvas = canvas;
   this.ctx = setupCanvas(this.canvas);
-
+  this.data = getIntervalData(workout);
 
   //sets the max value
   this.maxValue = 0;
-  for(x in data){
-    if(this.maxValue < data[x]){
-      this.maxValue = data[x];
+  for(x in this.data){
+    if(this.maxValue < this.data[x]){
+      this.maxValue = this.data[x];
     }
   }
 
@@ -85,18 +117,18 @@ let BarChart = function(canvas, data, gridScale, seriesName, padding, barGap){
 
   this.drawBars = function(){
     let barNo = 0;
-    let barWidth = canvasW / data.length;
+    let barWidth = canvasW / this.data.length;
     let barIncrement = (canvasH / this.maxValue) ;
 
-    for(x in data){
+    for(x in this.data){
       drawBar(
         this.ctx,
         barNo * barWidth + padding,
-        (this.maxValue - data[x])*barIncrement +padding ,
+        (this.maxValue - this.data[x])*barIncrement +padding ,
         barWidth - (barGap *2),
-        data[x] *barIncrement,
+        this.data[x] *barIncrement,
         getRandomColor(),
-        data[x],
+        this.data[x],
       );
       barNo++
     }
@@ -110,18 +142,7 @@ let BarChart = function(canvas, data, gridScale, seriesName, padding, barGap){
     this.ctx.font= "bold 40px Arial";
     this.ctx.fillText("Super chart", canvasW / 2, canvasH);
   }
-
-
-
-
-
 }
-
-let chart = new BarChart(chartArea, value, 2, "chart", 25, 10);
-console.log(chart.maxValue);
-chart.DrawGridLine();
-chart.drawBars();
-//chart.drawLabel();
 
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
