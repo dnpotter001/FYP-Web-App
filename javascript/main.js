4a7057b450c8720f4f8d0eb8c6b54057e18e7578
David Potter <dnpotter001@gmail.com>
2018-12-03 14:40:59 +0000

    making the layout better

--- javascript/main.js
+++ javascript/main.js
@@ -13,7 +13,6 @@ firebase.initializeApp(config);
 //menu elements
 const menuButton = document.getElementById("menuButton");
 const navbar = document.getElementById("navbar");
-const all = document.getElementById("all");
 
 //other ui elements
 const tableArea = document.getElementById("tables");
@@ -31,27 +30,9 @@ const pullButton = document.getElementById("PullFromFirebase");
 let workout = {};
 
 
-//event listeners
-menuButton.addEventListener("click", openClose);
-window.addEventListener("resize", resetCSS);
+menuButton.addEventListener("click", () => {
+  console.log("toggled");
+  navbar.classList.toggle("nav-open");
+  menuButton.classList.toggle("menu-open");
+});
 
-//displays and hides the side menu
-function openClose(){
-    if (navbar.style.width === "150px") {
-        navbar.style.width = "0";
-        all.style.marginLeft = "0";
-        menuButton.style.left =" 20px";
-    } else {
-        navbar.style.width = "150px";
-        all.style.marginLeft = "150px";
-        menuButton.style.left =" 170px";
-    }
-}
-
-//resets the css when whole browser is resized
-function resetCSS(){
-  navbar.style.width = "";
-  all.style.marginLeft = "";
-  menuButton.style.left ="";
-
-}
