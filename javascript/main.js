bfb1c66461671a8f7370074220ef8c8e4c22202b
David Potter <dnpotter001@gmail.com>
2018-12-06 17:08:15 +0000

    added smaller button sizing

--- javascript/main.js
+++ javascript/main.js
@@ -32,8 +32,6 @@ window.addEventListener("mouseup", (event) => {
   if(event.target != navbar && event.target != navbutton ){
     console.log("closing menu")
     navbar.classList.remove("nav-open");
- 
-
   }
 })
 
