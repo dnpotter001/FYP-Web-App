643247be9cdad3cfc4ffa1725fba8ce0e5d3b3fa
David Potter <dnpotter001@gmail.com>
2018-12-07 14:33:32 +0000

    added a generator function to feed.js that produces feed tiems

--- javascript/feed.js
+++ javascript/feed.js
@@ -2,9 +2,32 @@ const refreshFeed = document.getElementById("refresh-feed");
 
 refreshFeed.addEventListener("click", () => {
   console.log("refresh");
+  GenerateFeedItem();
 });
 
-function GenerateFeedItem(workout){
-  
+
+
+function GenerateFeedItem(){
+  let feedItem = document.createElement('article');
+  feedItem.className = 'feed-item';
+
+  let feedLabel = document.createElement('div');
+  feedLabel.className = 'feed label';
+
+  let feedGraph = document.createElement('figure');
+  feedGraph.className = 'feed graph' ;
+
+  let feedTable = document.createElement('article');
+  feedTable.className = 'feed table';
+
+  let feedInfo = document.createElement('div');
+  feedInfo.className ='feed info';
+
+  feedItem.appendChild(feedLabel);
+  feedItem.appendChild(feedGraph);
+  feedItem.appendChild(feedTable);
+  feedItem.appendChild(feedInfo);
+  document.getElementById('content').appendChild(feedItem);
+
 }
 
